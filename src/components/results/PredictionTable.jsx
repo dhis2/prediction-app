e89b69248c3a51353f4c27e875e775fb3c4c80ca
Bjørn Sandvik <bjorn@dhis2.org>
2024-04-25 18:04:40 +0200

    Prediction table

--- src/components/results/PredictionTable.jsx
+++ src/components/results/PredictionTable.jsx
@@ -1,5 +1,5 @@
 import i18n from "@dhis2/d2-i18n";
-import styles from "./styles/PredictionChart.module.css";
+import styles from "./styles/PredictionTable.module.css";
 
 const getValue = (values, ou, key) => {
   const item = values.find((v) => v.orgUnit === ou && v.dataElement === key);
@@ -10,23 +10,20 @@ const PredictionTable = ({ data }) => {
   const values = data.dataValues;
   const orgUnits = [...new Set(values.map((v) => v.orgUnit))];
 
-  console.log("orgUnits", orgUnits);
-
-  console.log(values);
   return (
-    <table>
+    <table className={styles.table}>
       <thead>
         <tr>
-          <th>Org unit</th>
-          <th>Median</th>
-          <th>Quantile low</th>
-          <th>Quantile high</th>
+          <th>{i18n.t("Org unit")}</th>
+          <th>{i18n.t("Median")}</th>
+          <th>{i18n.t("Quantile low")}</th>
+          <th>{i18n.t("Quantile high")}</th>
         </tr>
       </thead>
       <tbody>
         {orgUnits.map((ou) => (
           <tr key={ou}>
-            <td>{ou}</td>
+            <th>{ou}</th>
             <td>{getValue(values, ou, "median")}</td>
             <td>{getValue(values, ou, "quantile_low")}</td>
             <td>{getValue(values, ou, "quantile_high")}</td>
